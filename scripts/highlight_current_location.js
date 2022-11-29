1bad2f79578ce8e8587677cc8df2b7c7b23230e6
Sofia Vyatkina <sonia.vyatkina2@yandex.ru>
2022-11-29 17:02:59 +0300

    refactor: change script to highlight location

--- scripts/highlight_current_location.js
+++ scripts/highlight_current_location.js
@@ -12,4 +12,18 @@ function ready() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", ready);
+function ready1() {
+    const path = document.location.href;
+    let els = document.querySelectorAll('a');
+    for (let i = 0; i < els.length; i++) {
+        if (path === els[i].href) {
+            if (els[i].closest('aside') !== null) {
+                els[i].classList.add("active-aside");
+            } else {
+                els[i].classList.add("active-nav");
+            }
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", ready1);
