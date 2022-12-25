fbfe2661ded1b4bff223f980a4f237cd1c4fb7ff
Sofia Vyatkina <sonia.vyatkina2@yandex.ru>
2022-12-25 21:33:34 +0300

    feat: add validation

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -8,10 +8,21 @@ function addListElement(text) {
 function createListElement() {
     let task = document.getElementById('task').value;
     let task_time = document.getElementById('task_time').value;
+    if (task_time !== "") {
+        var d = new Date(task_time);
+        var t_date = d.toLocaleDateString();
+        var t_time = d.toLocaleTimeString().substring(0, 5);
+        task_time = `${t_date} ${t_time} `;
+    }
     let task_item = `${task_time} ${task}`;
-    itemsArray.push(task_item);
-    localStorage.setItem('items', JSON.stringify(itemsArray));
-    addListElement(task_item);
+    if (task !== "") {
+        itemsArray.push(task_item);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        addListElement(task_item);
+    }
+    else {
+        alert("Заполните поле 'Дело'!")
+    }
 }
 
 document.addEventListener("submit", createListElement);
@@ -24,6 +35,17 @@ document.ready = function()  {
         addListElement(item);
     });
 };
+
+$(function() {
+    var list = document.querySelector("#result_todo_list");
+    console.log(list);
+    list.addEventListener('click', function(ev) {
+        console.log(ev.target.tagName);
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
+});
 
 
 
